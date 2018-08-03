78cd4229226e439184df18f26d98275410b332be
Charles von Lehe <vonlehec@gmail.com>
2018-08-03 16:51:00 -0400

    Updated graph functionality

--- Jobcoin Tracker/Cells/GraphTableViewCell.swift
+++ Jobcoin Tracker/Cells/GraphTableViewCell.swift
@@ -26,10 +26,16 @@ class GraphTableViewCell: UITableViewCell {
     func populate () {
         //Generate bar chart data entries
         var entries:[BarChartDataEntry] = []
+        
+        //Track balance over time by adding or subtracting transaction amounts based on the recipient address
         let transactions:[Transaction] = User.current().transactions.reversed()
+        var balance:Float = 0
         for i in 0..<transactions.count {
-            entries.append(BarChartDataEntry(x: Double(i), y: Double(transactions[i].amount)))
+            let currentTransaction = transactions[i]
+            balance += currentTransaction.toAddress == User.current().address ? currentTransaction.amount : -currentTransaction.amount
+            entries.append(BarChartDataEntry(x: Double(i), y: Double(balance)))
         }
+
         
         //Create set based on entries
         let barDataSet = BarChartDataSet(values: entries, label: "")
@@ -40,6 +46,8 @@ class GraphTableViewCell: UITableViewCell {
         //Remove description label and legend
         barChartView.chartDescription?.text = ""
         barChartView.legend.enabled = false
+        barChartView.xAxis.drawLabelsEnabled = false
+        barChartView.leftAxis.drawLabelsEnabled = false
         
         //Set x axis labels to the bottom
         barChartView.xAxis.labelPosition = .bottom
